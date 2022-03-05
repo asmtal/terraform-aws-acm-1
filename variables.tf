5f87db17c63dd090b71cace26f010433818605f6
r0ck40k <benedikt@brauneck.org>
2022-03-05 08:36:09 +0100

    feat: added initial module files

--- variables.tf
+++ variables.tf
@@ -1,10 +1,14 @@
-variable "context" {
-  type = object({
-    organization = string
-    environment  = string
-    account      = string
-    product      = string
-    tags         = map(string)
-  })
-  description = "Default context variables"
+variable "domain" {
+  type        = string
+  description = "Domain to create ACM certificate for i.e. foo.bar"
+}
+variable "alternative_names" {
+  type        = list(string)
+  description = "List of alternative domain names i.e. *.foo.bar"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = ""
+  description = "Zone ID of the assigned Route53 Zone"
 }
